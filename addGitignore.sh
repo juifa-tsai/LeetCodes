610def5ae14df57d2d4f788122fcce7a5bcdcef2
Jui-Fa Tsai <alpha78178@gmail.com>
2018-03-05 23:02:12 +0800

    Add addGitignore

---
+++ addGitignore.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+lists=`ls -l ./* | grep '\-rwxr\-xr\-x' | grep -v addGitignore.sh | awk '{print $9}'`
+
+for file in $lists
+do
+    check=`grep $file .gitignore`
+    if [ ! $check ]; then
+        echo $file >> .gitignore 
+    fi
+done 
+
+#>> .gitignore
