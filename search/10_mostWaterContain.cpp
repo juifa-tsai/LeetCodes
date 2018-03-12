89ebd3e1dae024cf5e0911e4d184c6fbd668b5d7
Jui-Fa Tsai <alpha78178@gmail.com>
2018-03-12 12:00:33 +0800

    Update most water contain

--- search/10_mostWaterContain.cpp
+++ search/10_mostWaterContain.cpp
@@ -7,25 +7,39 @@
 #include <queue>
 #include <utility>
 using namespace std;
-
+/*
 // 01. O(n) : using priority queue
-    int maxArea(vector<int>& height) {
-        priority_queue<pair<int, int> > qHeight;
-        for( int i=0; i<height.size(); i++){
-            qHeight.push(make_pair(height[i], i));
-        }
-        int x_max = 0, x_min=height.size(), maxA=0;
-        while( !qHeight.empty() ){
-            x_max = max( x_max, qHeight.top().second );
-            x_min = min( x_min, qHeight.top().second );
-            maxA = max( qHeight.top().first*(x_max-x_min), maxA );
-            qHeight.pop();
-        }
-        return maxA;
+int maxArea(vector<int>& height) {
+    priority_queue<pair<int, int> > qHeight;
+    for( int i=0; i<height.size(); i++){
+        qHeight.push(make_pair(height[i], i));
+    }
+    int x_max = 0, x_min=height.size(), maxA=0;
+    while( !qHeight.empty() ){
+        x_max = max( x_max, qHeight.top().second );
+        x_min = min( x_min, qHeight.top().second );
+        maxA = max( qHeight.top().first*(x_max-x_min), maxA );
+        qHeight.pop();
+    }
+    return maxA;
+}
+*/
+
+// 02. O(n) Two Pointers Approach (contradiction)
+int maxArea(vector<int>& height) {
+    int maxA = 0;
+    int i = 0, j = height.size() - 1;
+    while(i < j)
+    {
+        maxA = max(maxA, min(height[i], height[j]) * (j - i));
+        if(height[i] < height[j])
+            i++;
+        else
+            j--;
     }
 
-// 02. O(n) Two Pointers Approach
-
+    return maxA;
+}
 
 int main(){
     vector<int> input;
