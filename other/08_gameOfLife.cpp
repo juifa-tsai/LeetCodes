1e6ee4d64e507d79d2220900ee617df57aa72435
Jui-Fa Tsai <alpha78178@gmail.com>
2018-03-21 09:57:10 +0800

    Update game of life

--- other/08_gameOfLife.cpp
+++ other/08_gameOfLife.cpp
@@ -6,6 +6,7 @@
 #include "../dataStructure/array.cpp"
 using namespace std;
 
+/*
 // 01. my code O(mn), space O(mn)
 void gameOfLife(vector<vector<int> >& board) {
     vector<vector<int> > copy = board;
@@ -26,6 +27,25 @@ void gameOfLife(vector<vector<int> >& board) {
         }
     }
     return;
+}
+*/
+
+// 02. time O(mn), space O(1)
+void gameOfLife(vector<vector<int> >& board) {
+    int m = board.size(), n = m ? board[0].size() : 0;
+    for (int i=0; i<m; ++i) {
+        for (int j=0; j<n; ++j) {
+            int count = 0;
+            for (int x=max(i-1, 0); x<min(i+2, m); ++x)
+                for (int y=max(j-1, 0); y<min(j+2, n); ++y)
+                    count += board[x][y] & 1;
+            if (count == 3 || count - board[i][j] == 3)
+                board[i][j] |= 2;
+        }
+    }
+    for (int i=0; i<m; ++i)
+        for (int j=0; j<n; ++j)
+            board[i][j] >>= 1;
 }
 
 int main(){
